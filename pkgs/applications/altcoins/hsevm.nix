89668dd04abddd0a783ef9488b5f3a1cef53cdc0
Mikael Brockman <mikael@brockman.se>
2017-09-01 18:04:17 +0300

    hsevm: v0.6.4 -> v0.7

--- pkgs/applications/altcoins/hsevm.nix
+++ pkgs/applications/altcoins/hsevm.nix
@@ -1,24 +1,25 @@
 { mkDerivation, abstract-par, aeson, ansi-wl-pprint, base
 , base16-bytestring, base64-bytestring, binary, brick, bytestring
 , cereal, containers, cryptonite, data-dword, deepseq, directory
-, filepath, ghci-pretty, here, HUnit, lens, lens-aeson, memory
-, monad-par, mtl, optparse-generic, process, QuickCheck
-, quickcheck-text, readline, rosezipper, scientific, stdenv, tasty, tasty-hunit
-, tasty-quickcheck, temporary, text, text-format
-, unordered-containers, vector, vty
+, filepath, ghci-pretty, gitlib, gitlib-libgit2, here, HUnit, lens
+, lens-aeson, memory, monad-par, mtl, optparse-generic, process
+, QuickCheck, quickcheck-text, readline, rosezipper, scientific
+, stdenv, tasty, tasty-hunit, tasty-quickcheck, temporary, text
+, text-format, time, unordered-containers, vector, vty
+
 , fetchFromGitHub, lib, makeWrapper
 , ncurses, zlib, bzip2, solc, coreutils
 }:
 
 lib.overrideDerivation (mkDerivation rec {
   pname = "hsevm";
-  version = "0.6.4";
+  version = "0.7";
 
   src = fetchFromGitHub {
     owner = "dapphub";
     repo = "hsevm";
     rev = "v${version}";
-    sha256 = "01b67k9cam4gvsi07q3vx527m1w6p6xll64k1nl27bc8ik6jh8l9";
+    sha256 = "0grsp2dziv6pvwx8y5i2m3079lwc67kccppm0szg5y69g3i2pzy7";
   };
 
   isLibrary = false;
@@ -36,7 +37,7 @@ lib.overrideDerivation (mkDerivation rec {
     cryptonite data-dword deepseq directory filepath ghci-pretty lens
     lens-aeson memory monad-par mtl optparse-generic process QuickCheck
     quickcheck-text readline rosezipper scientific temporary text text-format
-    unordered-containers vector vty
+    unordered-containers vector vty gitlib gitlib-libgit2
   ];
   executableHaskellDepends = [
     readline zlib bzip2
